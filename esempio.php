0659c53abf34a541adc771ae8fdd6720bc889c33
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2020-07-20 15:08:08 +0200

    upd

---
+++ esempio.php
@@ -0,0 +1,4 @@
+<h1>Pagina di esempio</h1>
+<a href="?" class="btn btn-primary">home page</a>
+<h2>Uso del database con PHP e PDO</h2>
+<a href="?pagina=elenco" class="btn btn-primary">Esempio DB con PDO</a>
