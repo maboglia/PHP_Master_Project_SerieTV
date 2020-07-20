326b8e63df203637720b1467dafa5f5c7f39d819
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2020-07-20 15:09:22 +0200

    ssa

--- home.php
+++
@@ -1,3 +0,0 @@
-<h1>Home page</h1>
-
-<a href="?pagina=esempio" class="btn btn-danger">esempio</a>
---
+++ view/home.php
@@ -0,0 +1,3 @@
+<h1>Home page</h1>
+
+<a href="?pagina=esempio" class="btn btn-danger">esempio</a>
